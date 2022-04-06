15f2ba40e7be91ee1e9155cd4fc67363b0229ad4
SiddharthShyniben <siddharth.muscat@gmail.com>
2022-04-06 11:30:57 +0530

    Configure github login

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import fetch from 'node-fetch';
 
 const PORT = process.env.PORT || 5000;
 
@@ -7,6 +8,25 @@ const app = express();
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
+
+app.get('/callback', async (req, res) => {
+	const {code} = req.query;
+
+	const login = await fetch('https://github.com/login/oauth/access_token', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			client_id: process.env.CLIENT_ID,
+			client_secret: process.env.CLIENT_SECRET,
+			code,
+		}),
+	}).then(res => res.json());
+
+	const accessToken = login.access_token;
+	res.send(accessToken)
+})
 
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
